fca6cfd757d3e57bb8bcb6b83cfc91335a1055e8
patangesomnath5-svg <patangesomnath5@gmail.com>
2026-06-12 15:01:12 +0530

    Fixed static file routing for HTML pages

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // ------------------------------
 // 1. MongoDB Connection
